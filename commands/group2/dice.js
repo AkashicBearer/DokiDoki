47f8407b57bc8f2d85406b845e3628e298692a76
AkashicBearer <35845153+AkashicBearer@users.noreply.github.com>
2018-03-14 21:08:37 +0200

    Create dice.js

---
+++ commands/group2/dice.js
@@ -0,0 +1,25 @@
+const { Command } = require('discord.js-commando');
+
+class DiceRollCommand extends  Command {
+    constructor(client) {
+        super(client, {
+            name: 'dice',
+            aliases: ['roll', 'rolldice', 'diceroll', 'rd'],
+            group: 'group2',
+            memberName: 'doce',
+            description: 'Roles a 6 Sided Dice.',
+            throttling: {
+                usages: 2,
+                duration: 1
+            },
+			
+        });
+    }
+async run(message, args) {
+    var roll = Math.floor(Math.random() * 6) + 1;
+    message.reply("You rolled a " + roll);
+}
+
+}
+
+module.exports = DiceRollCommand;
