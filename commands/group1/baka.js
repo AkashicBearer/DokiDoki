812af6763ededfab01021c40d911ac70f52470df
AkashicBearer <35845153+AkashicBearer@users.noreply.github.com>
2018-03-14 21:07:06 +0200

    Create baka.js

---
+++ commands/group1/baka.js
@@ -0,0 +1,31 @@
+const { Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+
+module.exports = class BakaCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'baka',
+            group: 'group1',
+            memberName: 'baka',
+            description: 'Mentions a User who is a Baka >,<',
+			args: [
+				{
+					key: 'member',
+					label: 'user',
+					prompt: 'Who is a Baka?',
+					type: 'member'
+				}
+			]
+        });
+    }
+	async run(msg, args) {
+		const member = args.member;
+        const user = member.user;
+        const embed = new RichEmbed()
+            .setDescription(args.member.user + " is a Being a Baka!")
+			.setImage("https://media.giphy.com/media/Gf3AUz3eBNbTW/giphy.gif")
+			.setColor(0x212121)
+        return msg.embed(embed);
+    }
+	
+};
