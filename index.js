3b73fe5cafec0339a4787fd69a0c9371b0947cb6
AkashicBearer <35845153+AkashicBearer@users.noreply.github.com>
2018-03-14 21:05:55 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+const { CommandoClient, SQLiteProvider } = require('discord.js-commando');
+const config = require("./config.json");
+const oneLine = require('common-tags').oneLine;
+const sqlite = require('sqlite');
+const path = require('path');
+const client = new CommandoClient({
+    commandPrefix: '<',
+    unknownCommandResponse: false,
+    owner: ['193021560792154112', '111469545637605376'],
+	developper: '339974470137544715',
+    disableEveryone: true
+});
+
+sqlite.open(path.join(__dirname, "settings.sqlite3")).then((db) => {
+    client.setProvider(new SQLiteProvider(db));
+});
+
+
+// Command Groups
+
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['group1', 'Emotion Commands'],
+        ['group2', 'Random Commands'],
+        ['group3', 'Bullshit Commands'],
+		['group4', 'User Commands'],
+		['group5', 'Administration Commands']
+    ])
+	
+// Console.Log and other stuff -.-
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+	client
+	.on('error', console.error)
+	.on('warn', console.warn)
+	.on('debug', console.log)
+	.on('ready', () => {
+		console.log(`Client ready; logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
+	})
+	.on('disconnect', () => { console.warn('Disconnected!'); })
+	.on('reconnecting', () => { console.warn('Reconnecting...'); })
+	.on('commandError', (cmd, err) => {
+		if(err instanceof commando.FriendlyError) return;
+		console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+	})
+	.on('commandBlocked', (msg, reason) => {
+		console.log(oneLine`
+			Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
+			blocked; ${reason}
+		`);
+	})
+	.on('commandPrefixChange', (guild, prefix) => {
+		console.log(oneLine`
+			Prefix ${prefix === '' ? 'removed' : `changed to ${prefix || 'the default'}`}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+	})
+	.on('commandStatusChange', (guild, command, enabled) => {
+		console.log(oneLine`
+			Command ${command.groupID}:${command.memberName}
+			${enabled ? 'enabled' : 'disabled'}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+	})
+	.on('groupStatusChange', (guild, group, enabled) => {
+		console.log(oneLine`
+			Group ${group.id}
+			${enabled ? 'enabled' : 'disabled'}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+	});
+	
+client.login(config.token);
