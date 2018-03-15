5db1b93c8d82ce8d03505414e84d7de3b68016d1
Kandrina <manuela.greifoner@gmx.at>
2018-03-15 22:41:19 +0100

    Pat Update

--- commands/group1/pat.js
+++ commands/group1/pat.js
@@ -22,11 +22,15 @@ module.exports = class PatCommand extends Command {
 	async run(msg, args) {
 		const member = args.member;
         const user = member.user;
+        var imgpat = {
+            "0": "https://thumbs.gfycat.com/BlankGiftedBurro-max-1mb.gif",
+            "1":"https://media1.tenor.com/images/1e92c03121c0bd6688d17eef8d275ea7/tenor.gif?itemid=9920853",
+            "2": "https://media.tenor.com/images/f79a9ec48bde0e592e55447b17ecfbad/tenor.gif"
+        };
         const embed = new RichEmbed()
             .setDescription('DokiDoki Pats ' + args.member.user)
-			.setImage("https://media1.tenor.com/images/1e92c03121c0bd6688d17eef8d275ea7/tenor.gif?itemid=9920853")
-			.setColor(0x23ff12)
+            .setImage(imgpat[Math.floor(Math.random() * Object.keys(imgpat).length).toString()])
+            .setColor(0x23ff12)
         return msg.embed(embed);
     }
-	
 };
