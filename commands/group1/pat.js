e74479b47f11ece5bb9745bfc3e03ce2d4629a48
Kandrina <manuela.greifoner@gmx.at>
2018-03-16 11:56:53 +0100

    Pat - almost

--- commands/group1/pat.js
+++ commands/group1/pat.js
@@ -51,7 +51,7 @@ module.exports = class PatCommand extends Command {
             "4": "http://i.imgur.com/uacfoA9.gif"
         };
         const embed = new RichEmbed()
-            .setDescription(msg.author.mention + ' DokiDoki pats ' + args.member.user)
+            .setDescription(msg.author + ' DokiDoki pats ' + args.member.user)
             .setImage(imgpat[Math.floor(Math.random() * Object.keys(imgpat).length).toString()])
             .setColor(0x23ff12)
         return msg.embed(embed);
