ea95c18306bd92c8bba8f19cd88468b172cb0201
Kandrina <manuela.greifoner@gmx.at>
2018-03-16 11:27:18 +0100

    I don't want to give up on selfpat

--- commands/group1/pat.js
+++ commands/group1/pat.js
@@ -51,8 +51,8 @@ module.exports = class PatCommand extends Command {
             "4": "http://i.imgur.com/uacfoA9.gif"
         };
         const embed = new RichEmbed()
-            .setDescription('DokiDoki pats ' + args.member.user)
-            .setImage(imgselfpat[Math.floor(Math.random() * Object.keys(imgselfpat).length).toString()])
+            .setDescription(mgs.author.username +' pats ' + args.member.user)
+            .setImage(imgspat[Math.floor(Math.random() * Object.keys(imgselfpat).length).toString()])
             .setColor(0x23ff12)    
             /*if(message.author.tag == args.member.user.tag){
                 embed.setDescription(message.author + ' pats... their own head, sometimes you just need a little bit love. ')
