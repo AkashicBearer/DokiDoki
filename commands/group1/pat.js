625a4f5078a34e72173de6e2cead7eec8637f77b
Kandrina <manuela.greifoner@gmx.at>
2018-03-16 11:29:32 +0100

    Pat

--- commands/group1/pat.js
+++ commands/group1/pat.js
@@ -51,7 +51,7 @@ module.exports = class PatCommand extends Command {
             "4": "http://i.imgur.com/uacfoA9.gif"
         };
         const embed = new RichEmbed()
-            .setDescription(mgs.author.username +' pats ' + args.member.user)
+            .setDescription(msg.author.username +' pats ' + args.member.user)
             .setImage(imgspat[Math.floor(Math.random() * Object.keys(imgselfpat).length).toString()])
             .setColor(0x23ff12)    
             /*if(message.author.tag == args.member.user.tag){
