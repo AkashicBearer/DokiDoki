8fa1fe1bef18341c44df3ddc3f97073b4ba0458f
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-03-16 09:23:42 +0200

    Update info.js

--- commands/group4/info.js
+++ commands/group4/info.js
@@ -15,11 +15,11 @@ module.exports = class InfoCommand extends Command {
         const embed = new RichEmbed()
 			.setTitle('DokiDoki Information')
             		.setDescription('My Information:')
-			.setDescription("My Discord Invite Link")
+			.addField("My Discord Invite Link")
 			.setUrl("https://discordapp.com/api/oauth2/authorize?client_id=385115460397694977&permissions=8&scope=bot")
-         		.addDescription("My Github Documentation")
+         		.addField("My Github Documentation")
 	    		.setUrl("https://github.com/AkashicBearer/DokiDoki")
-			.addDescription("DokiDoki Runs on Discird.JS-Commando")
+			.addField("DokiDoki Runs on Discird.JS-Commando")
 			.setColor(0x23ff12)
         return msg.embed(embed);
     }
