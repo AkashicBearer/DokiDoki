d55c75c48ebbf5d240cc753bbcb4b807e42b566b
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-03-17 20:37:15 +0200

    Create ban.js

---
+++ commands/group5/ban.js
@@ -0,0 +1,38 @@
+const { Commando } = require('discord.js-commando');
+
+module.exports = class BanCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'ban',
+            aliases: ['Ban', 'bean', 'banuser'],
+            group: 'group5',
+            memberName: 'ban',
+            description: 'Bans a User',
+            examples: ['{Prefix}Ban @User#0000'],
+            guildOnly: true,
+            clientPermissions: ['ADMINISTRATOR', "BAN_MEMBERS"],
+            throttling: {
+                usages: 2,
+                duration: 10
+            },
+            args: [
+                { 
+                 key: 'member',
+                 label: 'user',
+                 prompt: 'Who to Ban?',
+                 type: 'member'
+                },
+                {
+                 key: "text",
+                 prompt: "Why are you Banning this user?",
+                 type: "string"
+                 }
+                  ]
+});
+
+async run(msg, args, ) {
+guild.ban(args.member.user, {reason: args.text })
+  .then(console.log, + msg.channel.send(args.member.user + "Was Banned")
+  .catch(console.error);
+  }
+};
