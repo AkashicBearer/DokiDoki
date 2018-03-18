e184d5867bac2d43a02d0ca6e245328eb4692b7f
Kandrina <manuela.greifoner@gmx.at>
2018-03-18 13:41:31 +0100

    quote

--- commands/group2/quote.js
+++ commands/group2/quote.js
@@ -20,9 +20,9 @@ module.exports = class QuoteCommand extends Command {
         });
     }
 	async run(msg, args) {
-        const msgid = msg.channel.fetchMessage(args.idx)
+       // const msgid = msg.channel.fetchMessage(args.idx)
         const embed = new RichEmbed()
-            .setDescription(msgid.content)
+            .setDescription(args.idx.content)
             .setColor(0x23ff12)
         return msg.embed(embed);
     }
