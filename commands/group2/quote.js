f36882a1f051d20a52d5c516b75b3424c0229be8
Kandrina <manuela.greifoner@gmx.at>
2018-03-18 11:46:48 +0100

    quote

--- commands/group2/quote.js
+++ commands/group2/quote.js
@@ -24,7 +24,7 @@ module.exports = class QuoteCommand extends Command {
         const embed = new RichEmbed()
 //            embed.setThumbnaul(args.id,author.avatar)
 //            embed.setAuthor(args.id.author)
-            embed.setDescription(args.id)
+            embed.setDescription(msgid.content)
             embed.setColor(0x23ff12)
         return msg.embed(embed);
     }
