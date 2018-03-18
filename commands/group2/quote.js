c4084ef83776768b87289e527887536d924ec0f3
Kandrina <manuela.greifoner@gmx.at>
2018-03-18 11:30:47 +0100

    Create quote.js

---
+++ commands/group2/quote.js
@@ -0,0 +1,30 @@
+const { Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+
+module.exports = class QuoteCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'quote',
+            group: 'group1',
+            memberName: 'quote',
+            description: 'What message do you want to quote?',
+          
+			args: [
+				{
+					key: 'id',
+					label: 'user',
+					prompt: 'What messáge do you want to quote? (ID)',
+					type: 'message'
+				}
+			]
+        });
+    }
+	async run(msg, args) {
+        const embed = new RichEmbed()
+        embed.setThumbnaul(args.id,author.avatar)
+            embed.setAuthor(args.id.author)
+            embed.setDescription(args.is.content)
+            embed.setColor(0x23ff12)
+        return msg.embed(embed);
+    }
+};
