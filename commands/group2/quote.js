d2e15df875e71e91406b65b12501423ccb37f7b7
Kandrina <manuela.greifoner@gmx.at>
2018-03-18 12:05:48 +0100

    quote

--- commands/group2/quote.js
+++ commands/group2/quote.js
@@ -20,11 +20,11 @@ module.exports = class QuoteCommand extends Command {
         });
     }
 	async run(msg, args) {
-        const msgid = msg.channel.fetchMessage(args.id);
+        const msgid = msg.channel.fetchMessage(args.id).id;
         const embed = new RichEmbed()
             //embed.setThumbnail(msgid.author.avatar)
             //embed.setAuthor(msgid.author)
-            embed.setDescription(msgid.id.content)
+            embed.setDescription(msgid.content)
             embed.setColor(0x23ff12)
         return msg.embed(embed);
     }
