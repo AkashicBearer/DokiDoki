7bd10410836c2762c76f9a52525f8ad9be539877
Kandrina <manuela.greifoner@gmx.at>
2018-03-19 01:49:43 +0100

    testdice

--- commands/group2/testdice.js
+++ commands/group2/testdice.js
@@ -7,12 +7,21 @@ module.exports = class TestdiceCommand extends Command {
             name: 'testdice',
             group: 'group1',
             memberName: 'testdice',
-            description: 'Sends a Nico Nico Nii!'
+            description: 'Sends a Nico Nico Nii!',
+            args: [
+                {
+                    key: 'xsides',
+                    label: 'sides',
+                    prompt: 'How many sides does the dice have?',
+                    type: 'float',
+                    default: 6
+                }
+            ]
         });
     }
     async run(msg, args) {
         const embed = new RichEmbed()
-            .setDescription('Nico Nico Nii!')
+            .setDescription(args.xsides)
             .setImage("https://i.imgur.com/lqSm7gk.gif")
             .setColor(0x212121)
         return msg.embed(embed);
