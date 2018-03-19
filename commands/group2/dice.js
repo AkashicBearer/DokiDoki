c4c1788004313a879d77df1593deeb0aac1b9893
Kandrina <manuela.greifoner@gmx.at>
2018-03-19 02:00:28 +0100

    dice

--- commands/group2/dice.js
+++ commands/group2/dice.js
@@ -1,19 +1,19 @@
 const { Command } = require('discord.js-commando')
 const { RichEmbed } = require('discord.js');
 
-module.exports = class DiceRollCommand extends  Command {
+module.exports = class DiceRollCommand extends Command {
     constructor(client) {
         super(client, {
             name: 'dice',
             aliases: ['roll', 'rolldice', 'diceroll', 'rd'],
             group: 'group2',
             memberName: 'dice',
-            description: 'Roles a 6 Sided Dice.',
+            description: 'Roles a Dice with provided sides.',
             throttling: {
                 usages: 2,
                 duration: 1
             },
-			 args: [
+            args: [
                 {
                     key: 'xsides',
                     label: 'sides',
@@ -24,9 +24,14 @@ module.exports = class DiceRollCommand extends  Command {
             ]
         });
     }
-async run(message, args) {
-    var roll = Math.floor(Math.random() * args.xsides) + 1;
-    message.reply("You rolled a " + roll);
-}
-
-}
+    async run(msg, args) {
+        const roll = Math.floor(Math.random() * args.xsides) + 1;
+        const embed = new RichEmbed()
+            .setAuthor(msg.author.username, msg.author.avatarURL)
+            .setTitle("Rolling " + args.xsides + " sided dice.")
+            .setDescription("You rolled a " + roll)
+            .setThumbnail("https://gilkalai.files.wordpress.com/2017/09/dice.png?w=640")
+            .setColor(0x212121)
+        return msg.embed(embed);
+    } 
+};
