43fa3dcb15353a3ee8aa38384d16a5a41de00081
Kandrina <manuela.greifoner@gmx.at>
2018-03-19 01:25:16 +0100

    dice

--- commands/group2/dice.js
+++ commands/group2/dice.js
@@ -1,13 +1,12 @@
 const { Command } = require('discord.js-commando');
-const { RichEmbed } = require('discord.js');
 
-module.exports = class DiceRollCommand extends  Command {
+class DiceRollCommand extends  Command {
     constructor(client) {
         super(client, {
             name: 'dice',
             aliases: ['roll', 'rolldice', 'diceroll', 'rd'],
             group: 'group2',
-            memberName: 'dice',
+            memberName: 'doce',
             description: 'Roles a 6 Sided Dice.',
             throttling: {
                 usages: 2,
@@ -24,15 +23,13 @@ module.exports = class DiceRollCommand extends  Command {
             ]
         });
     }
+async run(message, args) {
+    var roll = Math.floor(Math.random() * args.xsides) + 1;
+    message.reply("You rolled a " + roll);
+}
 
-async run(msg, args) {
-        const roll = Math.floor(Math.random() * args.xsides) + 1
-        const embed = new RichEmbed()
-            embed.setDescription("You rolled a " + roll)
-            embed.setColor(0x212121)
-        return message.embed(embed);
-    } 
+
 
 }
 
-//module.exports = DiceRollCommand;
+module.exports = DiceRollCommand;
