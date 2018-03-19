216a20c8a04cb4aa7670361515c2eb5fb1a61df3
Kandrina <manuela.greifoner@gmx.at>
2018-03-19 20:03:59 +0100

    testhelp

--- commands/group4/testhelp.js
+++ commands/group4/testhelp.js
@@ -20,11 +20,41 @@ module.exports = class TestHelpCommand extends Command {
     }
     async run(msg, args) {
         const groups = this.client.registry.groups.first(7);
-        const commands = this.client.registry.findCommands(args.command, false, msg);
+        const commands = this.client.registry.findCommands().first(51);
         const showAll = args.command && args.command.toLowerCase() === 'all';
+        var grp1 = "";
+        var grp2 = "";
+        var grp3 = "";
+        var grp4 = "";
+        var grp5 = "";
+        var grp6 = "";
+        var grp7 = "";
+        for(var i = 0; i < 51; i++){
+            if(commands[i].group.name == groups[0]){
+                grp1=grp1+commands[i].name+", ";
+            }
+            if(commands[i].group.name == groups[1]){
+                grp2=grp2+commands[i].name+", ";
+            }
+            if(commands[i].group.name == groups[2]){
+                grp3=grp3+commands[i].name+", ";
+            }
+            if(commands[i].group.name == groups[3]){
+                grp4=grp4+commands[i].name+", ";
+            }
+            if(commands[i].group.name == groups[4]){
+                grp5=grp5+commands[i].name+", ";
+            }
+            if(commands[i].group.name == groups[5]){
+                grp6=grp6+commands[i].name+", ";
+            }
+            if(commands[i].group.name == groups[6]){
+                grp7=grp7+commands[i].name+", ";
+            }
+        }
         const embed = new RichEmbed()
                 .setTitle('DokiDoki Commands')
-                .setDescription(groups[0].name + "")
+                .addField(groups[0].name + "",grp1)
             .setColor(0x23ff12)
         return msg.embed(embed);
     }
