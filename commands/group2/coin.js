8dc2294815f35146ffb8f48cfe90e93dc21aa7c8
Kandrina <manuela.greifoner@gmx.at>
2018-03-19 11:48:38 +0100

    Added coin.js

---
+++ commands/group2/coin.js
@@ -0,0 +1,34 @@
+const { Command } = require('discord.js-commando')
+const { RichEmbed } = require('discord.js');
+
+module.exports = class CoinFlipCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'coin',
+            aliases: ['flip', 'coinflip', 'flipcoin'],
+            group: 'group2',
+            memberName: 'coin',
+            description: 'Flips a coin',
+            throttling: {
+                usages: 2,
+                duration: 1
+            }
+        });
+    }
+    async run(msg, args) {
+        const flips = Math.floor(Math.random() * 100) + 1;
+        const embed = new RichEmbed()
+            embed.setAuthor(msg.author.username, msg.author.avatarURL)
+            embed.setTitle("Flipping a coin.")
+
+            if(flips<50){
+                embed.setDescription("It landed on tails!")
+                embed.setThumbnail("https://imgur.com/YgITIS6")
+            }else{
+                embed.setDescription("It landed on head!")
+                embed.setThumbnail("https://imgur.com/PlEnG9I")
+            }
+            embed.setColor(0x212121)
+        return msg.embed(embed);
+    } 
+};
