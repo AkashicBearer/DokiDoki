f75dc4e4c34683971c5b56a8898b7cef49efbbbd
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-03-20 21:35:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -75,6 +75,6 @@ client.registry
 	});
 // Random Shits
 
-// client.user.setGame('with ${this.client.guilds.size}', { type: 'Playing' })
+client.user.setActivity('with ' + {this.client.guilds.size})
 
 client.login(process.env.token);
