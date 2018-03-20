000fe58b3d4a0b5803475cdb3db409c3fcae87c4
Kandrina <manuela.greifoner@gmx.at>
2018-03-21 00:38:09 +0100

    th

--- commands/group4/testhelp.js
+++ commands/group4/testhelp.js
@@ -83,15 +83,19 @@ module.exports = class TestHelpCommand extends Command {
                 embed.setTitle('DokiDoki Commands')
                 if(grp1c.length > 0){
                     embed.addField(groups.find('id','group1').name+"",grp1+"\n")
+                    embed.addField(" ", " ")
                 }
                 if(grp2c.length > 0){
                     embed.addField(groups.find('id','group2').name+"",grp2+"\n")
+                    embed.addField(" ", " ")
                 }
                 if(grp3c.length > 0){
                     embed.addField(groups.find('id','group3').name+"",grp3+"\n")
+                    embed.addField(" ", " ")
                 }
                 if(grp4c.length > 0){
                     embed.addField(groups.find('id','group4').name+"",grp4+"\n")
+                    embed.addField(" ", " ")
                 }
                 if(grp5c.length > 0){
                     embed.addField(groups.find('id','group5').name+"",grp5+"\n")
