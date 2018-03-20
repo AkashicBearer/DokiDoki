c1c472a40b12ee6c3c2b343c96011baf8725e541
Kandrina <manuela.greifoner@gmx.at>
2018-03-20 16:44:03 +0100

    th

--- commands/group4/testhelp.js
+++ commands/group4/testhelp.js
@@ -19,12 +19,19 @@ module.exports = class TestHelpCommand extends Command {
         });
     }
     async run(msg, args) {
-        const groups = this.client.registry.groups.findAll();
+        const groups = this.client.registry.groups;
         const commands = this.client.registry.findCommands();
-        const showAll = args.command && args.command.toLowerCase() === 'all';
         const grp1c = commands.findAll('groupID','group1');
+        const grp2c = commands.findAll('groupID','group2');
+        const grp3c = commands.findAll('groupID','group3');
+        const grp4c = commands.findAll('groupID','group4');
+        const grp5c = commands.findAll('groupID','group5');
 
         var grp1 = "";
+        var grp2 = "";
+        var grp3 = "";
+        var grp4 = "";
+        var grp5 = "";
 
         for(var i = 0; i < grp1c.length; i++){
             grp1=grp1+"`"+grp1c[i].name+"`";
@@ -32,9 +39,42 @@ module.exports = class TestHelpCommand extends Command {
                 grp1=grp1+", ";
             }
         }
+
+        for(var i = 0; i < grp2c.length; i++){
+            grp2=grp2+"`"+grp2c[i].name+"`";
+            if(i+1 < grp2c.length){
+                grp2=grp2+", ";
+            }
+        }
+
+        for(var i = 0; i < grp3c.length; i++){
+            grp3=grp3+"`"+grp3c[i].name+"`";
+            if(i+1 < grp3c.length){
+                grp3=grp3+", ";
+            }
+        }
+
+        for(var i = 0; i < grp4c.length; i++){
+            grp4=grp4+"`"+grp4c[i].name+"`";
+            if(i+1 < grp4c.length){
+                grp4=grp4+", ";
+            }
+        }
+
+        for(var i = 0; i < grp5c.length; i++){
+            grp5=grp5+"`"+grp5c[i].name+"`";
+            if(i+1 < grp5c.length){
+                grp5=grp5+", ";
+            }
+        }
+
         const embed = new RichEmbed()
                 .setTitle('DokiDoki Commands')
                 .addField(groups.find('id','group1').name,grp1)
+                .addField(groups.find('id','group2').name,grp2)
+                .addField(groups.find('id','group3').name,grp3)
+                .addField(groups.find('id','group4').name,grp4)
+                .addField(groups.find('id','group5').name,grp5)
             .setColor(0x23ff12)
         return msg.embed(embed);
     }
