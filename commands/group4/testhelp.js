96f607a744f49d8004a7b2525b84de368b87b43a
Kandrina <manuela.greifoner@gmx.at>
2018-03-20 23:29:21 +0100

    th

--- commands/group4/testhelp.js
+++ commands/group4/testhelp.js
@@ -21,17 +21,9 @@ module.exports = class TestHelpCommand extends Command {
     async run(msg, args) {
         const groups = this.client.registry.groups;
         const commands = this.client.registry.findCommands();
-        const grp1c = commands.findAll('groupID','group1');
-        const grp2c = commands.findAll('groupID','group2');
-        const grp3c = commands.findAll('groupID','group3');
-        const grp4c = commands.findAll('groupID','group4');
-        const grp5c = commands.findAll('groupID','group5');
 
+        const grp1c = commands.findAll('groupID','group1');
         var grp1 = "";
-        var grp2 = "";
-        var grp3 = "";
-        var grp4 = "";
-        var grp5 = "";
 
         for(var i = 0; i < grp1c.length; i++){
             grp1=grp1+"`"+grp1c[i].name+"`";
@@ -40,6 +32,9 @@ module.exports = class TestHelpCommand extends Command {
             }
         }
 
+/*
+        const grp2c = commands.findAll('groupID','group2');
+        var grp2 = "";
         for(var i = 0; i < grp2c.length; i++){
             grp2=grp2+"`"+grp2c[i].name+"`";
             if(i+1 < grp2c.length){
@@ -47,6 +42,8 @@ module.exports = class TestHelpCommand extends Command {
             }
         }
 
+        const grp3c = commands.findAll('groupID','group3');
+        var grp3 = "";
         for(var i = 0; i < grp3c.length; i++){
             grp3=grp3+"`"+grp3c[i].name+"`";
             if(i+1 < grp3c.length){
@@ -54,6 +51,8 @@ module.exports = class TestHelpCommand extends Command {
             }
         }
 
+        const grp4c = commands.findAll('groupID','group4');
+        var grp4 = "";
         for(var i = 0; i < grp4c.length; i++){
             grp4=grp4+"`"+grp4c[i].name+"`";
             if(i+1 < grp4c.length){
@@ -61,20 +60,22 @@ module.exports = class TestHelpCommand extends Command {
             }
         }
 
+        const grp5c = commands.findAll('groupID','group5');
+        var grp5 = "";
         for(var i = 0; i < grp5c.length; i++){
             grp5=grp5+"`"+grp5c[i].name+"`";
             if(i+1 < grp5c.length){
                 grp5=grp5+", ";
             }
         }
-
+*/
         const embed = new RichEmbed()
                 .setTitle('DokiDoki Commands')
                 .addField(groups.find('id','group1').name,grp1)
-                .addField(groups.find('id','group2').name,grp2)
-                .addField(groups.find('id','group3').name,grp3)
-                .addField(groups.find('id','group4').name,grp4)
-                .addField(groups.find('id','group5').name,grp5)
+            //    .addField(groups.find('id','group2').name,grp2)
+            //    .addField(groups.find('id','group3').name,grp3)
+            //    .addField(groups.find('id','group4').name,grp4)
+            //    .addField(groups.find('id','group5').name,grp5)
             .setColor(0x23ff12)
         return msg.embed(embed);
     }
