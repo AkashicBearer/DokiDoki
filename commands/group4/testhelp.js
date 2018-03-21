9fc7d9c79c26feeab4f9b7aac77c76b268938689
Kandrina <manuela.greifoner@gmx.at>
2018-03-21 01:37:04 +0100

    th

--- commands/group4/testhelp.js
+++ commands/group4/testhelp.js
@@ -94,7 +94,7 @@ module.exports = class TestHelpCommand extends Command {
                     }
                     embed.addField("Aliases",aliass+" ")
 
-                    if(cmd.examples){
+                    if(cmd.examples != null){
                         for(var i = 0; i < cmd.examples.length;i++){
                             examp=examp+"`"+cmd.examples[i]+"`";
                             if(i+1 < cmd.examples.length){
