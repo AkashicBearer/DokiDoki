14b59dfe87eba79d2c66e6545358964446ef738c
Kandrina <manuela.greifoner@gmx.at>
2018-03-22 14:37:43 +0100

    bug

--- commands/group4/bug.js
+++ commands/group4/bug.js
@@ -20,7 +20,7 @@ module.exports = class BugCommand extends Command {
         });
     }
     async run(msg, args) {
-      const chann = client.guilds.find("name", "Doki Doki Server");
+      //const chann = client.guilds.find("name", "Doki Doki Server");
       msg.channel.send('Sent');
     }
 };
