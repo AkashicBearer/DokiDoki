e5068cdc9ad92525e9ef2ab9a5fa227d3b17e527
Kandrina <manuela.greifoner@gmx.at>
2018-03-23 13:28:47 +0100

    shrug

--- commands/group1/shrug.js
+++ commands/group1/shrug.js
@@ -22,7 +22,7 @@ async run(msg, args) {
             "5": "https://1.bp.blogspot.com/-GySgfFQ6Xl8/WciIBt8bSyI/AAAAAAAA8K0/TtDUGoDLng4i_wxQ70VYYq7tOF9LfOETgCKgBGAs/s1600/Omake%2BGif%2BAnime%2B-%2BKakegurui%2B-%2BEpisode%2B12%2B%255BEND%255D%2B-%2BKirari%2BShrugs.gif",
             "6": "https://i.pinimg.com/originals/bb/5b/35/bb5b355ba22a387436c74625f9de205f.gif",
             "7": "https://img.fireden.net/a/image/1487/35/1487357313092.gif",
-            "8": "https://steamusercontent-a.akamaihd.net/ugc/870744308638282986/1ED9693AE9300ED411B99C556C3D489EE43C8D72/",
+            "8": "http://gifimage.net/wp-content/uploads/2017/09/anime-shrug-gif-2.gif",
             "9": "https://78.media.tumblr.com/0cf5b8479cc687456e29e23287910445/tumblr_p1edjjqx7m1wn2b96o1_500.gif",
             "10": "https://thumbs.gfycat.com/MedicalDazzlingAzurewingedmagpie-size_restricted.gif",
             "11": "https://media.tenor.com/images/6def41cfbfc28b3be8c20be9d6ef2dde/tenor.gif",
