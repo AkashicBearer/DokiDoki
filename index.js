13a7a033456b306030e6ca1954950750924fdece
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-03-28 15:47:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,48 +37,10 @@ client.registry
 })
     .registerCommandsIn(path.join(__dirname, 'commands'));
   client	 
-	  .on('error', console.error)
+	.on('error', console.error)
 	.on('warn', console.warn)
 	.on('debug', console.log)
-	.on('ready', () => {
-		console.log(`Client ready; logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
-	  	client.user.setActivity(`with ${client.guilds.size} Servers`);
-	  	//setInterval(() => {
-		//dbl.postStats(client.guilds.size);
-   		 //}, 1800000);
-	})
-	.on('disconnect', () => { console.warn('Disconnected!'); })
-	.on('reconnecting', () => { console.warn('Reconnecting...'); })
-	.on('commandError', (cmd, err) => {
-		if(err instanceof commando.FriendlyError) return;
-		console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
-	})
-	.on('commandBlocked', (msg, reason) => {
-		console.log(oneLine`
-			Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
-			blocked; ${reason}
-		`);
-	})
-	.on('commandPrefixChange', (guild, prefix) => {
-		console.log(oneLine`
-			Prefix ${prefix === '' ? 'removed' : `changed to ${prefix || 'the default'}`}
-			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
-		`);
-	})
-	.on('commandStatusChange', (guild, command, enabled) => {
-		console.log(oneLine`
-			Command ${command.groupID}:${command.memberName}
-			${enabled ? 'enabled' : 'disabled'}
-			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
-		`);
-	})
-	.on('groupStatusChange', (guild, group, enabled) => {
-		console.log(oneLine`
-			Group ${group.id}
-			${enabled ? 'enabled' : 'disabled'}
-			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
-		`);
-	});
+
 // Random Shits
 
 
