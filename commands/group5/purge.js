941991f21ab632884793486b92a0ab17ae0d0f63
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-03-31 20:28:49 +0200

    Create purge.js

---
+++ commands/group5/purge.js
@@ -0,0 +1,37 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class BanCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'purge',
+            aliases: ['clear', 'clean', ],
+            group: 'group5',
+            memberName: 'purge',
+            description: 'purge messages',
+            examples: ['{Prefix}purge <number> '],
+            guildOnly: true,
+            clientPermissions: ['ADMINISTRATOR', MANAGE_MESSAGES],
+            throttling: {
+                usages: 2,
+                duration: 10
+            },
+            args: [
+                {
+                 key: "number",
+                 prompt: "How many Messages to Clear?",
+                 type: "float",
+                 default: '2'
+                 }
+                  ]
+});
+}
+    
+async run(msg, args, ){
+    if(msg.member.hasPermission('ADMINISTRATOR', MANAGE_MESSAGES)){   
+    args.message.delete(args.number)
+    msg.channel.send(args.number + "Wascleared");
+    }else{
+        msg.channel.send("You need to be Admin to use this");
+    }
+  }
+};
