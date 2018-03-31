a3214fde79657c4c98f116ab985f172551e51c1a
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-03-31 20:31:33 +0200

    Update purge.js

--- commands/group5/purge.js
+++ commands/group5/purge.js
@@ -27,8 +27,8 @@ module.exports = class BanCommand extends Command {
 }
     
 async run(msg, args, ){
-    if(msg.member.hasPermission('ADMINISTRATOR', MANAGE_MESSAGES)){   
-    args.message.delete(args.number)
+    if(msg.member.hasPermission('ADMINISTRATOR', 'MANAGE_MESSAGES')){   
+    message.delete(args.number)
     msg.channel.send(args.number + "Wascleared");
     }else{
         msg.channel.send("You need to be Admin to use this");
