ca3791a03710179abe30ac82a2bc05ad7883cb8f
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-03-31 22:19:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ client.registry
 	.on('error', console.error)
 	.on('warn', console.warn)
 	.on('debug', console.log)
+	.on('ready', () => {
+		console.log(`Client ready; logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
+	  	client.user.setActivity(`with ${client.guilds.size} Servers`);
+	  	//setInterval(() => {
+		//dbl.postStats(client.guilds.size);
+   		 //}, 1800000);
+	})
 
 // Random Shits
 
