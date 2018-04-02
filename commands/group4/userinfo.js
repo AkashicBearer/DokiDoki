c14b9f0aee1f1a3e50cbe892a8eca9cb49d9316b
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-04-02 12:25:04 +0200

    Update userinfo.js

--- commands/group4/userinfo.js
+++ commands/group4/userinfo.js
@@ -29,8 +29,8 @@ module.exports = class UserInfoCommand extends Command {
 	const embed = new RichEmbed()
 		embed.setTitle(user.username + ' User info')
 		embed.setDescription(' User info of ' + user.username)
-		embed.addField(`Username: ${user.username}`, `Nickname: ${member.nickname}`, `Joined at: ${member.joinedAt}` false)
-		embed.addField(`User Details`, `Account Create at: ${user.createdAt}`, `Activity: ${user.presence.status}`, `Playing: ${user.presence.game ? user.presence.game.name : 'None'}` false)
+		embed.addField(`Username: ${user.username}`, `Nickname: ${member.nickname}`, `Joined at: ${member.joinedAt}`, false)
+		embed.addField(`User Details`, `Account Create at: ${user.createdAt}`, `Activity: ${user.presence.status}`, `Playing: ${user.presence.game ? user.presence.game.name : 'None'}`, false)
 		embed.setThumbnail(args.member.user.avatarURL)
 		/*		**❯ Member Details**
 			${member.nickname !== null ? ` • Nickname: ${member.nickname}` : ' • No nickname'}
