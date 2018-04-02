aefd0cf715a9ff94877af7ea3c09ae3943a5cfe8
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-04-02 12:09:12 +0200

    Update userinfo.js

--- commands/group4/userinfo.js
+++ commands/group4/userinfo.js
@@ -1,5 +1,5 @@
-const stripIndents = require('common-tags').stripIndents;
 const { Command } = require('discord.js-commando')
+const { RichEmbed } = require('discord.js');
 
 module.exports = class UserInfoCommand extends Command {
 	constructor(client) {
@@ -26,10 +26,12 @@ module.exports = class UserInfoCommand extends Command {
 	async run(msg, args) {
         const member = args.member;
         const user = member.user;
-		return msg.reply(stripIndents`
-			Info on **${user.username}#${user.discriminator}** (ID: ${user.id})
-
-			**❯ Member Details**
+	const embed = new RichEmbed()
+		embed.setTitle(user.username + ' User info')
+		embed.setDescription(' User info of ' + user.username)
+		embed.addField(`Username: ${user.username}`, `Nickname: ${member.nickname}`), `Joined at: ${member.joinedAt}`)
+		embed.addField(`User Details`, `Account Create at: ${user.createdAt}`, `Activity: ${user.presence.status}`, `Playing: ${user.presence.game ? user.presence.game.name : 'None'}`
+/*		**❯ Member Details**
 			${member.nickname !== null ? ` • Nickname: ${member.nickname}` : ' • No nickname'}
 			 • Roles: ${member.roles.map(roles => `\`${roles.name}\``).join(', ')}
 			 • Joined at: ${member.joinedAt}
@@ -38,6 +40,7 @@ module.exports = class UserInfoCommand extends Command {
 			 • Created at: ${user.createdAt}${user.bot ? '\n • Is a bot account' : ''}
 			 • Status: ${user.presence.status}
 			 • Game: ${user.presence.game ? user.presence.game.name : 'None'}
-        `);
+        `);*/
+	return msg.embed(embed);
 };
 };
