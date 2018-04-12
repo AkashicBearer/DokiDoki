f9936b279a006c49a308d10f05eb59d6bba0ee78
Kandrina <manuela.greifoner@gmx.at>
2018-04-12 20:27:10 +0200

    c

--- commands/group4/anime.js
+++ commands/group4/anime.js
@@ -56,7 +56,7 @@ mal.anime.search('naruto')
   	embed.addField("Title", res.title,true)
   	embed.addField("English Title", res.english, true)
   	embed.addField("Description", res.synopsis.toString().replace(/<.*>/g,' ').replace(/&#039;/g,"'"))
-  	embed.setThumbnail(img)
+  	embed.setThumbnail(res.image.toString().substring(res.image.toString().indexOf("'")+1,res.image.toString().lastIndexOf("'")))
    	msg.channel.send(embed)   
   }
 
