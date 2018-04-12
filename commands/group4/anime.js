9dde296d440e56cc83b17b8bcc6dd41d75acc8a2
Kandrina <manuela.greifoner@gmx.at>
2018-04-12 20:49:42 +0200

    c

--- commands/group4/anime.js
+++ commands/group4/anime.js
@@ -57,6 +57,41 @@ const mal = new MALjs('DokiDokiBot', 'DokiDoki');
             "11": "November",
             "12": "December"
         }
+     const days = {
+            "01": "1st",
+            "02": "2nd",
+            "03": "3rd",
+            "04": "4th",
+            "05": "5th",
+            "06": "6th",
+            "07": "7th",
+            "08": "8th",
+            "09": "9th",
+            "10": "10th",
+            "11": "11th",
+            "12": "12th",
+            "13": "13th",
+            "14": "14th",
+            "15": "15th",
+            "16": "16th",
+            "17": "17th",
+            "18": "18th",
+            "19": "19th",
+            "20": "20th",
+            "21": "21st",
+            "22": "22nd",
+            "23": "23rd",
+            "24": "24th",
+            "25": "25th",
+            "26": "26th",
+            "27": "27th",
+            "28": "28th",
+            "29": "29th",
+            "30": "30th",
+            "32": "31st"
+        }
+
+
     var anm = args.name+'';
 
   	var embed = new RichEmbed()
@@ -78,7 +113,7 @@ mal.anime.search(anm)
   	var fromspl = res.start_date.toString().split('-');
   	var from = months[fromspl[2]] + " " + fromspl[1] + " " + fromspl[0];
   	var tospl = res.end_date.toString().split('-');
-  	var to = months[tospl[2]] + " " + tospl[1] + " " + tospl[0];
+  	var to = months[tospl[2]] + " " + days[tospl[1]] + ", " + tospl[0];
 
   	embed.setFooter(from + " to " + to)
   	embed.setThumbnail(res.image.toString())
