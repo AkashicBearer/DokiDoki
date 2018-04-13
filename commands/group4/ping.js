939900b147da571da1ebdc71fbb8fa04ca49eac1
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-04-13 15:42:45 +0200

    Update ping.js

--- commands/group4/ping.js
+++ commands/group4/ping.js
@@ -16,7 +16,7 @@ module.exports = class PingCommand extends Command {
      const embed = new RichEmbed()
         embed.setAuthor('DokiDoki')
         embed.setTitle('Client Ping')
-        embed.setDescription(`The Ping is ${Math.round(client.ping)} ms`)
+        embed.setDescription(`The Ping is ${client.ping}ms`)
         embed.setThumbnail('https://emojipedia-us.s3.amazonaws.com/thumbs/160/emoji-one/44/table-tennis-paddle-and-ball_1f3d3.png')
         embed.setColor(0x23ff12)
         embed.setTimestamp()
