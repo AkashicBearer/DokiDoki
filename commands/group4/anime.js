f9c108fb34b0ae756bcf56ebb87366a505c5483f
Kandrina <manuela.greifoner@gmx.at>
2018-04-13 09:38:20 +0200

    anime

--- commands/group4/anime.js
+++ commands/group4/anime.js
@@ -112,11 +112,12 @@ mal.anime.search(anm)
   				titarr.push(result.anime[i].id)
   				firstrun = false;
   			}
-  			titlenbuff = titarr.length;
+  			msg.channel.send(titarr.length)
   			for (var j = 0; j < titlenbuff; i++) {
   				if(idbuff == titarr[j]){
-  					//skip
+  					msg.channel.send("id same")
   				}else{
+  					msg.channel.send("id not same")
   					titles = titles + "**["+ (i+1) + "]** " + result.anime[i].title + "\n";
   					titarr.push(result.anime[i].id)
   				}
