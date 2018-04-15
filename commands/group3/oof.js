29ccb3b2d1d139247c5b84ca6469e68cb80e2e1f
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-04-15 23:24:21 +0200

    Update oof.js

--- commands/group3/oof.js
+++ commands/group3/oof.js
@@ -41,6 +41,5 @@ module.exports = class OofCommand extends Command {
           embed.setImage(oof[Math.floor(randm * Object.keys(oof).length).toString()])
           embed.setColor(0x23ff12)
        return msg.embed(embed);
-          }
-}
-);
+          })
+};
