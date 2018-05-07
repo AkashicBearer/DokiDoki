c038f192a694f9d4f0aaf5b1af947cc1d4a5e394
Kandrina <manuela.greifoner@gmx.at>
2018-05-07 20:25:56 +0200

    tr

--- commands/group4/translate.js
+++ commands/group4/translate.js
@@ -62,7 +62,7 @@ async run(msg, args){
                 })   
 
             }else{
-                translate(`${txt}`, {to: `${to}`}}).then(res => {
+                translate(`${txt}`, {to: `${to}`}).then(res => {
                     const embed = new RichEmbed()
                     embed.setColor('RANDOM');
                     console.log(res);
