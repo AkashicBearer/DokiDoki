2093d18014b3344a1e21a95c275bc8c3d85596e8
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-05-16 00:08:42 +0200

    Create commandshtml.js

---
+++ docs/js/commandshtml.js
@@ -0,0 +1,14 @@
+//Commands.html
+function openCom(evt, command) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(command).style.display = "block";
+    evt.currentTarget.className += " active";   
+}
