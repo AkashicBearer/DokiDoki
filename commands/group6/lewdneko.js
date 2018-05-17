66f26068608396ef7b4d65d9ce06075c651ef77b
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-05-17 14:04:06 +0200

    Update lewdneko.js

--- commands/group6/lewdneko.js
+++ commands/group6/lewdneko.js
@@ -11,19 +11,29 @@ module.exports = class LewdNekoCommand extends Command {
             aliases: ['ln'],
             group: 'group6',
             memberName: 'lewdneko',
-            description: 'Sends a NSFW Neko',
+            description: 'Sends a NSFW Neko use -g for gif',
+            examples: ["<ln -g", "<ln"]
         });
     }
 
   async run(message, msg, neko) {
-    let {body} = await superagent
-    .get(`https://nekos.life/api/lewd/neko`);
-    if (!message.channel.nsfw) return message.reply("You can use this command only in a **NSFW** Channel");
-  
+    if(msg.indexOf("-g")){
+    const {body} = await superagent
+    .get(`https://nekos.life/api/v2/img/neko`);
+    if (!message.channel.nsfw) return message.reply("You can use this command only on nsfw channels!");
     const embed = new RichEmbed()
         embed.setColor("RANDOM")
         embed.setTitle("Lewd Neko")
-        embed.setImage(body.neko)
+        embed.setImage(body.url)
     message.channel.send(embed);
-}
+    }else{
+    const {body} = await superagent 
+    .get('https://nekos.life/api/v2/img/nsfw_neko_gif')
+    if (!message.channel.nsfw) return message.reply("You can only use this comand in a NSFW Channel");
+        const embed1 = new RichEmbed()
+            embed1.setColor("RANDOM")
+            embed1.setTitle("Lewd Neko GIF")
+            embed1.setImage(body.url)
+        message.channel.send(embed1);
+}}
 }
