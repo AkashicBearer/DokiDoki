e859586f55f6cdba6e4453cf384ad11e3b3901a3
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-05-17 18:50:52 +0200

    Create randomavatar.js

---
+++ commands/group3/randomavatar.js
@@ -0,0 +1,26 @@
+const { Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+const neko = require("nekos.life");
+const superagent = require("superagent");
+const send = require("quick.hook");
+
+module.exports = class RandomAvatarCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'randomavatar',
+            aliases: ['ra'],
+            group: 'group3',
+            memberName: 'randomavatar',
+            description: 'Sends a Random Avatar Image',
+        });
+    }
+	async run(msg, args, neko) { 
+	const {body} = await superagent 
+        .get('https://nekos.life/api/v2/img/avatar')
+        const embed = new RichEmbed()
+	        embed.setTitle('Here is your Random Avatar')
+            embed.setImage(body.url)
+            embed.setColor('RANDOM')
+        return msg.embed(embed);
+	}
+};
