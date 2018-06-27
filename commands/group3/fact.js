50cfa95cba675bd9125fdbccddafa0ed45e5933a
Kandrina <manuela.greifoner@gmx.at>
2018-06-27 08:41:49 +0200

    made stuff prettier

--- commands/group3/fact.js
+++ commands/group3/fact.js
@@ -15,11 +15,12 @@ module.exports = class FactCommand extends Command {
         });
     }
 	async run(msg, args, neko) { 
-	const {body} = await superagent 
+	const body = await superagent 
         .get('https://nekos.life/api/v2/fact')
         const embed = new RichEmbed()
 	        embed.setTitle('Random Fact~')
-            embed.setDescription(`Did you Know: ${body.fact}`)
+            embed.setDescription('Did you Know: ' + body.fact.charAt(0).toUpperCase + body.fact.slice(1))
+            embed.setThumbnail('https://upload.wikimedia.org/wikipedia/commons/2/2e/Exclamation_mark_red.png')
             embed.setColor('RANDOM')
         return msg.embed(embed);
 	}
