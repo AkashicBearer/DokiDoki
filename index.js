5b1be2ebc2d6c2295210ca45dba46f57bfd38b51
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-08-12 17:44:07 +0200

    asd

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ if (!usersOnCooldown.has(message.author.id)){
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;     
   const { Pool } = require ('pg');
-  const pool = new Pool({ connectionString: process.env.HEROKU_POSTGRESQL_GREEN_URL, 
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL, 
        port: 5432, 
        host: process.env.dbhost, 
        database: process.env.pool, 
