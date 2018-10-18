5f0d8131df05938b74b59d140b65f8f48c866817
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-10-18 08:47:55 +0200

    Update timezone.js

--- commands/settings/timezone.js
+++ commands/settings/timezone.js
@@ -1,4 +1,4 @@
-const { Command } = require('discord.js-commando');
+/*const { Command } = require('discord.js-commando');
 const { RichEmbed } = require('discord.js');
 
 
@@ -59,3 +59,4 @@ async run(msg, args){
     });
   };
 };
+*/
