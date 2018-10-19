5360b2e059560383ee5c160286d2bd04814055f7
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-10-19 12:34:32 +0200

    Update cuddle.js

--- commands/emo/cuddle.js
+++ commands/emo/cuddle.js
@@ -2,6 +2,7 @@ const { Command } = require('discord.js-commando');
 const { RichEmbed } = require('discord.js');
 const neko = require("nekos.life");
 const superagent = require("superagent");
+const send = require("quick.hook");
 
 module.exports = class CuddleCommand extends Command {
     constructor(client) {
@@ -10,29 +11,45 @@ module.exports = class CuddleCommand extends Command {
             aliases: [],
             group: 'emo',
             memberName: 'cuddle',
-            description: 'Have a cuddle',
-            args:[
+            description: 'Cuddle a user!',
+            args: [
                 {
                     key: 'member',
-                    prompt: 'Which user do you want to cuddle?',
+                    prompt: 'Who to cuddle?',
+                    type: 'member',
+                },
+                {
+                    key: 'stuff',
+                    prompt: 'What would u like to say?',
                     type: 'string',
+                    default: '',
+                    validate: stuff => {
+                        if (stuff.length < 201) return true;
+                        return 'Message Content is above 200 characters';
+                    }
                 }
             ]
         });
     }
-	async run(msg, args, neko) { 
-    
-   superagent.get('https://nekos.life/api/v2/img/cuddle')
+async run(msg, args, neko) {
+        superagent.get('https://nekos.life/api/v2/img/cuddle')
         .then(body => {
             body = body.body
-            const embed = new RichEmbed()
-            embed.setDescription(msg.author + ' cuddles ' + args.member)
-            embed.setImage(body.url)
-            embed.setColor('RANDOM')
-             return msg.embed(embed);
+        const cuddle = new RichEmbed()
+            if(msg.author.id == args.member.id){
+                cuddle.setAuthor(`${msg.author.username} is cuddling themselves?!`)
+            }else {
+                cuddle.setAuthor(`${msg.author.username} is cuddling ${args.member.user.username}!`)
+            }
+                    
+            cuddle.setDescription(args.stuff)
+            cuddle.setImage(body.url)
+            cuddle.setColor(0x23ff12)
+            cuddle.setFooter(`Powered by Nekos.Life`)
+        msg.channel.send(cuddle)
         })
         .catch(err => {
             msg.channel.send("The gif-API is currently down, plese try again later \nOr try to help us get to 200 Servers so we can upgrade our API!")
         })
-    }
+    }   
 };
