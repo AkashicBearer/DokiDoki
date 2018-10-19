c74c178c9d84e171e33bf2d2ad49d78122d6dfc3
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-10-19 12:31:15 +0200

    Update baka.js

--- commands/emo/baka.js
+++ commands/emo/baka.js
@@ -1,5 +1,8 @@
 const { Command } = require('discord.js-commando');
 const { RichEmbed } = require('discord.js');
+const neko = require("nekos.life");
+const superagent = require("superagent");
+const send = require("quick.hook");
 
 module.exports = class BakaCommand extends Command {
     constructor(client) {
@@ -12,55 +15,44 @@ module.exports = class BakaCommand extends Command {
 			args: [
 				{
 					key: 'member',
-					label: 'user',
 					prompt: 'Who is a Baka?',
 					type: 'member',
                     default: ''
-				}
+				},
+                {
+                    key: 'stuff',
+                    prompt: 'What would u like to say?',
+                    type: 'string',
+                    default: '',
+                    validate: stuff => {
+                        if (stuff.length < 201) return true;
+                        return 'Message Content is above 200 characters';
+                    }
+                }
 			]
         });
     }
-	async run(msg, args) {
-        var imgbaka = {
-            "0": "http://i0.kym-cdn.com/photos/images/newsfeed/001/028/204/4d9.gif",
-            "1":"https://media.giphy.com/media/Gf3AUz3eBNbTW/giphy.gif",
-            "2": "https://im-01.gifer.com/79UU.gif",
-            "3": "https://media1.tenor.com/images/99ed452a33c03a91c37083df7eb2419f/tenor.gif?itemid=5586968",
-            "4": "https://im-01.gifer.com/8TJ8.gif",
-            "5": "http://i0.kym-cdn.com/photos/images/newsfeed/001/093/416/a84.gif",
-            "6": "https://thumbs.gfycat.com/CapitalDarlingBlackcrappie-size_restricted.gif",
-            "7": "http://data.whicdn.com/images/71874842/large.gif",
-            "8": "https://im-01.gifer.com/FiiA.gif",
-            "9": "http://i.imgur.com/Uv675gV.gif",
-            "10": "https://orig00.deviantart.net/3ac0/f/2011/216/c/6/you__re_an_idiot__by_jeditears09-d45gqs2.gif",
-            "11": "http://i1152.photobucket.com/albums/p483/Thomas_Daniell/Titled%20A/Alice%20and%20Zoroku/Alice%20and%20Zouroku%203b_zpsdl3ifcvt.gif",
-            "12": "https://78.media.tumblr.com/7ade33279099cd6961639dae722b3e3a/tumblr_inline_o6j95hFvDB1rfvphd_500.gif",
-            "13": "http://4.bp.blogspot.com/-5p_CGTcnEPY/VV1vF1HfM_I/AAAAAAAALOA/g4XsC-lYctc/s1600/ccc.gif",
-            "14": "https://theedschronicles.files.wordpress.com/2017/01/zsjsbkgkoqbhy.gif",
-            "15": "http://pa1.narvii.com/6438/e559cbd92bc98a84f3e0de1636e0a5676e8b703a_hq.gif",
-            "16": "http://pa1.narvii.com/5811/aa04cddc78229d07cba9a1fb68d40730121e42b9_hq.gif",
-            "17": "https://i1.wp.com/i0.kym-cdn.com/photos/images/original/000/813/103/5c4.gif?resize=596%2C339",
-            "18": "https://media1.tenor.com/images/7a73c890a73adaf7a461bc555542869d/tenor.gif?itemid=5589406",
-            "19": "https://78.media.tumblr.com/9637a4ec073d3865d14668015cef92c1/tumblr_oqfmzhAVA51shbny2o1_500.gif",
-            "20": "http://gifimage.net/wp-content/uploads/2017/09/baka-gif-11.gif"
-        };
-
-        const embed = new RichEmbed();
-         if(args.member){
+async run(msg, args, neko) {
+        superagent.get('https://nekos.life/api/v2/img/baka')
+        .then(body => {
+            body = body.body
+        const baka = new RichEmbed()
             if(msg.author.id == args.member.id){
-                embed.setDescription(msg.author + ' is a baka, because they are calling themselves baka')
-             }else{
-                embed.setDescription(msg.author + ' thinks ' + args.member.user + ' is a baka.')
-             }
-         }else{
-            embed.setDescription('Someone has been a real baka.')
-         }
-         
-         const randm = Math.random();
-            embed.setImage(imgbaka[Math.floor(randm * Object.keys(imgbaka).length).toString()])
-
-            embed.setColor(0x23ff12)
-        return msg.embed(embed);
-    }
-	
+                baka.setAuthor(`${msg.author.username} thinks they are a baka!`)
+            }else if(!args.member){
+                baka.setAuthor(`Someone is being a Baka!`)
+            } else {
+                baka.setAuthor(`${msg.author.username} thinks ${args.member.user.username} is a baka!`)
+            }
+                    
+            baka.setDescription(args.stuff)
+            baka.setImage(body.url)
+            baka.setColor(0x23ff12)
+            baka.setFooter(`Powered by Nekos.Life`)
+        msg.channel.send(baka)
+        })
+        .catch(err => {
+            msg.channel.send("The gif-API is currently down, plese try again later \nOr try to help us get to 200 Servers so we can upgrade our API!")
+        })
+    }	
 };
