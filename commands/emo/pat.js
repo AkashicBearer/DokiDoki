a7b462a3467768b7e7ba51c5317b197968295db0
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-10-21 15:45:11 +0200

    Update pat.js

--- commands/emo/pat.js
+++ commands/emo/pat.js
@@ -2,6 +2,7 @@ const { Command } = require('discord.js-commando');
 const { RichEmbed } = require('discord.js');
 const neko = require("nekos.life");
 const superagent = require("superagent");
+const send = require("quick.hook");
 
 module.exports = class PatCommand extends Command {
     constructor(client) {
@@ -10,43 +11,57 @@ module.exports = class PatCommand extends Command {
             aliases: [],
             group: 'emo',
             memberName: 'pat',
-            description: 'Have a pat',
-            args:[
+            description: 'Pat a user!',
+            args: [
                 {
                     key: 'member',
-                    prompt: 'Which user do you want to pat?',
+                    prompt: 'Who to pat?',
+                    type: 'member',
+                },
+                {
+                    key: 'stuff',
+                    prompt: 'What would u like to say?',
                     type: 'string',
+                    default: '',
+                    validate: stuff => {
+                        if (stuff.length < 201) return true;
+                        return 'Message Content is above 200 characters';
+                    }
                 }
             ]
         });
     }
-	async run(msg, args, neko) { 
-       var imgselfpat = {
+async run(msg, args, neko) {
+    if(msg.author.id == args.member.id){
+        var imgselfpat = {
             "0": "https://pa1.narvii.com/6401/655b40f33530a90101682ee74c5fa12a673df749_hq.gif",
             "1": "https://i.redd.it/2ohfjanym13z.gif",
             "2": "https://i.imgur.com/65yP14R.gif",
             "3": "https://pa1.narvii.com/5790/0556780a813c3f6d93b0b178187bca7cec5b68dd_hq.gif",
             "4": "http://i.imgur.com/uacfoA9.gif"
-        };
-
+         };
+         const randm = Math.random();
+         const pat1 = new RichEmbed()
+            pat1.setAuthor(`${msg.author.username} is Patting themselves.`)
+            pat1.setImage(imgselfpat[Math.floor(randm * Object.keys(imgselfpat).length).toString()])
+            pat1.setColor(0x23ff12)
+            pat1.setFooter(`Powered by Nekos.Life`)
+        msg.channel.send(pat1)
+    } else { 
         superagent.get('https://nekos.life/api/v2/img/pat')
         .then(body => {
             body = body.body
-            const embed = new RichEmbed()
-           if(msg.author == args.member){
-               embed.setDescription(msg.author + ' pats.. their own head.. everyone needs a little bit of love sometimes ')
-               const randm = Math.random();
-               embed.setImage(imgselfpat[Math.floor(randm * Object.keys(imgselfpat).length).toString()])
-            }else{
-                embed.setDescription(msg.author + ' is Patting ' + args.member)
-                embed.setImage(body.url)
-                }
-               embed.setColor("RANDOM")
-           return msg.channel.send(embed);
+        const pat = new RichEmbed()
+            pat.setAuthor(`${msg.author.username} is giving a pat to ${args.member.user.username}!`)
+            pat.setImage(body.url)                    
+            pat.setDescription(args.stuff)
+            pat.setColor(0x23ff12)
+            pat.setFooter(`Powered by Nekos.Life`)
+        msg.channel.send(pat)
         })
         .catch(err => {
             msg.channel.send("The gif-API is currently down, plese try again later \nOr try to help us get to 200 Servers so we can upgrade our API!")
         })
-    
-}
+    }
+    }   
 };
