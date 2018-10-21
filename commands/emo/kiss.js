3d0e1f6fcf4909d731670e2bde747b7c0a1c996b
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-10-21 15:41:53 +0200

    Update kiss.js

--- commands/emo/kiss.js
+++ commands/emo/kiss.js
@@ -4,37 +4,52 @@ const neko = require("nekos.life");
 const superagent = require("superagent");
 const send = require("quick.hook");
 
-module.exports = class kissCommand extends Command {
+module.exports = class KissCommand extends Command {
     constructor(client) {
         super(client, {
             name: 'kiss',
             aliases: ['chu'],
             group: 'emo',
             memberName: 'kiss',
-            description: 'Who do you want to kiss?',
+            description: 'Kiss a user!',
             args: [
                 {
-                key: 'member',
-                label: 'user',
-                prompt: 'Who to kiss?',
-                type: 'member'
+                    key: 'member',
+                    prompt: 'Who to kiss?',
+                    type: 'member',
+                },
+                {
+                    key: 'stuff',
+                    prompt: 'What would u like to say?',
+                    type: 'string',
+                    default: '',
+                    validate: stuff => {
+                        if (stuff.length < 201) return true;
+                        return 'Message Content is above 200 characters';
+                    }
                 }
             ]
         });
     }
-    async run(msg, args, neko) { 
+async run(msg, args, neko) {
         superagent.get('https://nekos.life/api/v2/img/kiss')
         .then(body => {
             body = body.body
-            const embed = new RichEmbed()
-            embed.setDescription(msg.author + ' kisses ' + args.member)
-            embed.setImage(body.url)
-            embed.setColor('RANDOM')
-             return msg.embed(embed);
+        const kiss = new RichEmbed()
+            if(msg.author.id == args.member.id){
+                kiss.setAuthor(`${msg.author.username} is kissing themselves?!`)
+            }else {
+                kiss.setAuthor(`${msg.author.username} is kissing ${args.member.user.username} <3`)
+            }
+                    
+            kiss.setDescription(args.stuff)
+            kiss.setImage(body.url)
+            kiss.setColor(0x23ff12)
+            kiss.setFooter(`Powered by Nekos.Life`)
+        msg.channel.send(kiss)
         })
         .catch(err => {
             msg.channel.send("The gif-API is currently down, plese try again later \nOr try to help us get to 200 Servers so we can upgrade our API!")
         })
-       
-    }
+    }   
 };
