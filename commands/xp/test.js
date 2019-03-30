facd80868260a12613356af4f2b9f43ec8444854
Akashic Bearer <kurihararei1@gmail.com>
2019-03-30 16:37:13 +0100

    Just le me fking go live

--- commands/xp/test.js
+++ commands/xp/test.js
@@ -1,4 +1,4 @@
-let { Command } = require('discord.js-commando');
+/*let { Command } = require('discord.js-commando');
 let { RichEmbed } = require('discord.js');
 
 module.exports = class TestCommand extends Command {
@@ -54,3 +54,4 @@ const connection  = mysql.createPool({
 })
 }
 }
+*/
