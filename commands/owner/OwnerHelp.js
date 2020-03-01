b4c8f0164e6c33b4494323ce9c7f7d1bc453a87b
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2020-03-01 15:14:20 +0100

    v.0.0.9aa
    Updated Beta to v0.0.9aa
    - Removed almost all emotion commands (since useless on betabit)
    + fixed heal?
    -  fixed some broken commands

--- commands/owner/OwnerHelp.js
+++ commands/owner/OwnerHelp.js
@@ -25,11 +25,13 @@ module.exports = class OwnerHelpCommand extends Command {
     }
 
     async run(message, args) {
+
         const groups = this.client.registry.groups;
         const commands = this.client.registry.findCommands();
 
-        const oxp = commands.findAll('groupID','ownerxp');
+        const oxp = commands.filter(group => group.groupID === 'ownerxp');
         var oxp1 = "";
+
         if(oxp.length > 0){
             for(var i = 0; i < oxp.length; i++){
                 oxp1=oxp1+"`"+oxp[i].name+"`";
@@ -39,8 +41,9 @@ module.exports = class OwnerHelpCommand extends Command {
             }
         }
 
-        const own = commands.findAll('groupID','owner');
+        const own = commands.filter(group => group.groupID === 'owner');
         var own2 = "";
+
         if(own.length > 0){
             for(var i = 0; i < own.length; i++){
                 own2=own2+"`"+own[i].name+"`";
@@ -51,12 +54,17 @@ module.exports = class OwnerHelpCommand extends Command {
         }
 
         const embed = new RichEmbed()
-                if(commands.find('name',args.command)){
-                    const cmd = this.client.registry.findCommands().find(command => command.name, args.command);
+
+                if(commands.filter(command => command.name === args.command)){
+
+                    const cmd =  this.client.registry.findCommands().filter(command => command.name === args.command);
+
                     embed.setTitle("Help for " + cmd.name)
                     embed.addField("Description", cmd.description+" ")
+
                     var aliass = "";
                     var examp = "";
+
                     if(cmd.aliases.length > 0){
                         for(var i = 0; i < cmd.aliases.length;i++){
                             aliass=aliass+"`"+cmd.aliases[i]+"`";
@@ -64,34 +72,47 @@ module.exports = class OwnerHelpCommand extends Command {
                                 aliass=aliass+", ";
                             }
                         }
+
                     embed.addField("Aliases",aliass+" ")
+
                     }
 
                     if(cmd.examples != null){
+
                         for(var i = 0; i < cmd.examples.length;i++){
                             examp=examp+"`"+cmd.examples[i]+"`";
                             if(i+1 < cmd.examples.length){
                                 examp=examp+", ";
                             }
                         }
+
                         embed.addField("Examples",examp+" ")
+
                     }
                 
                 }else{
-                    embed.setTitle('DokiDoki Owner Commands')
+
+                    embed.setTitle('Owner Commands')
                     embed.setThumbnail(this.client.user.avatarURL)
-                    embed.setFooter("Owner Only Commands v1")
+                    embed.setFooter("Owner Commands v1")
 
                     if(oxp.length > 0){
-                        embed.addField(groups.find(group => group.id === 'ownerxp').name+"",oxp1+" ")
+
+                        embed.addField(groups.filter(groups => groups.groupID === 'ownerxp').name+"",oxp1+" ")
+                    
                     }
                         embed.addBlankField()
+
                     if(own.length > 0){
-                        embed.addField(groups.find(group => group.id === 'owner').name+"",own2+" ")
+
+                        embed.addField(groups.filter52362263263326(groups => groups.groupID === 'owner').name+"",own2+" ")
+                    
                     }
                 }
                 
             embed.setColor("RED")
+
         return message.embed(embed);
+
     }
 }
